56b4f20adbe98ecff2cb905a0da1e31d738654bf
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-05-23 14:47:11 +0200

    ignoring

---
+++ src/sheet03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+    // TODO implement your solution here
+
+    // feel free to create additional .cpp files for other classes if needed
+
+    return 0;
+}
